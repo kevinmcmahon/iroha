e047912a1fceaf033298a9c2cdee9d70da2f87c5
Nikita Alekseev <n.alekseev2104@gmail.com>
2018-07-23 14:04:11 +0300

    Remove unused test
    Signed-off-by: Nikita Alekseev <n.alekseev2104@gmail.com>
    
    # Conflicts:
    #	shared_model/backend/protobuf/common_objects/proto_common_objects_factory.hpp
    #	shared_model/interfaces/common_objects/common_objects_factory.hpp
    #	test/module/shared_model/backend_proto/common_objects/proto_common_objects_factory_test.cpp

--- irohad/ordering/impl/ordering_gate_transport_grpc.hpp
+++ irohad/ordering/impl/ordering_gate_transport_grpc.hpp
@@ -18,8 +18,8 @@
 #define IROHA_ORDERING_GATE_TRANSPORT_GRPC_H
 
 #include <google/protobuf/empty.pb.h>
-#include <backend/protobuf/proto_proposal_factory.hpp>
 
+#include "backend/protobuf/proto_proposal_factory.hpp"
 #include "logger/logger.hpp"
 #include "network/impl/async_grpc_client.hpp"
 #include "network/ordering_gate_transport.hpp"
@@ -56,7 +56,8 @@ namespace iroha {
       std::weak_ptr<iroha::network::OrderingGateNotification> subscriber_;
       std::unique_ptr<proto::OrderingServiceTransportGrpc::Stub> client_;
       std::shared_ptr<shared_model::proto::ProtoProposalFactory<
-          shared_model::validation::DefaultProposalValidator>> factory_;
+          shared_model::validation::DefaultProposalValidator>>
+          factory_;
     };
 
   }  // namespace ordering
